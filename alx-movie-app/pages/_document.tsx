73147e0349805a06e9463c311d49349ce22e7cb7
Mitchshimy <mitchshimy@gmail.com>
2025-07-02 12:37:24 +0300

    Link the Manifest

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link the manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+
+        {/* Optional: PWA mobile icons */}
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
